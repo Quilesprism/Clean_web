d492dddc62e73924e8614d8abfdfe586128719a9
analistaDatos365 <riesgos365@omenlaceglobal.co>
2023-11-08 11:28:58 -0500

    arreglos en alarmas

--- clean/alarmas.py
+++ clean/alarmas.py
@@ -18,15 +18,17 @@ def guardar_alarmas_y_promedio(df):
     df['PROMEDIO'] = df.groupby('No. DOCUMENTO DE IDENTIDAD')['VALOR DE LA TRANSACCION'].transform('mean')
     # Crear la columna ALARMAS con valores por defecto NORMAL
     df['ALARMAS'] = 'USUAL'
+    #cambiar a sospechoso si la transacción es mayor a 10 millones
+    df.loc[df['VALOR DE LA TRANSACCION'] > 10000000, 'ALARMAS'] = 'SOSPECHOSA_NORMAL'
     # Cambiar a INUSUAL si alguna transacción es al menos cinco veces mayor al promedio
     df.loc[df['VALOR DE LA TRANSACCION'] >= 5 * df['PROMEDIO'], 'ALARMAS'] = 'INUSUAL'
     # Cambiar a SOSPECHOSA si además la transacción se realizó con MEDIO PAGO EFECTIVO
-    df.loc[(df['ALARMAS'] == 'INUSUAL') & (df['MEDIO PAGO'] == 'EFECTIVO'), 'ALARMAS'] = 'SOSPECHOSA'    
-    #cambiar a sospechoso si la transacción es mayor a 10 millones
-    df.loc[df['VALOR DE LA TRANSACCION'] > 10000000, 'ALARMAS'] = 'SOSPECHOSA'
+    df.loc[(df['ALARMAS'] == 'INUSUAL') & (df['MEDIO PAGO'] == 'EFECTIVO'), 'ALARMAS'] = 'SOSPECHOSA_EFECTIVO'    
+
+    
     carpeta = 'alarmas'
-    columnas_deseadas = ['No. DOCUMENTO DE IDENTIDAD', 'NOMBRE', 'VALOR DE LA TRANSACCION', 'MEDIO PAGO', 'TIPO PERSONA (natural/jurídica)', 'CANAL DE DISTRIBUCION', 'ALARMAS']
-    df_filtrado = df[df['ALARMAS'].isin(['INUSUAL', 'SOSPECHOSA'])]
+    columnas_deseadas = ['FECHA TRANSACCION', 'No. DOCUMENTO DE IDENTIDAD', 'NOMBRE', 'VALOR DE LA TRANSACCION', 'PROMEDIO', 'MEDIO PAGO', 'TIPO PERSONA (natural/jurídica)', 'CANAL DE DISTRIBUCION', 'ALARMAS']
+    df_filtrado = df[df['ALARMAS'].isin(['INUSUAL', 'SOSPECHOSA_NORMAL', 'SOSPECHOSA_EFECTIVO'])]
     df_filtrado = df_filtrado[columnas_deseadas]
     fecha_actual = timezone.now()
     nombre_archivo = f'Alarmas_{fecha_actual.strftime("%Y%m%d%H%M%S")}.xlsx'
@@ -39,13 +41,13 @@ def guardar_alarmas_y_promedio(df):
     return nombre_archivo
 
 def guardar_alarmasP(df):
-    df['PROMEDIO'] = df.groupby('No. DOCUMENTO DE IDENTIDAD')['VALOR DE LA TRANSACCION'].transform('mean')
+    df['PROMEDIO'] = df.groupby('FECHA TRANSACCION', 'No. DOCUMENTO DE IDENTIDAD')['VALOR DE LA TRANSACCION'].transform('mean')
     df['ALARMAS'] = 'USUAL'
     df.loc[df['VALOR DE LA TRANSACCION'] >= 5 * df['PROMEDIO'], 'ALARMAS'] = 'INUSUAL'
     df.loc[(df['ALARMAS'] == 'INUSUAL') & (df['MEDIO PAGO'] == 'EFECTIVO'), 'ALARMAS'] = 'SOSPECHOSA'    
     df.loc[df['VALOR DE LA TRANSACCION'] > 10000000, 'ALARMAS'] = 'SOSPECHOSA'
     carpeta = 'alarmas'
-    columnas_deseadas = ['No. DOCUMENTO DE IDENTIDAD', 'NOMBRE', 'VALOR DE LA TRANSACCION', 'MEDIO PAGO', 'TIPO PERSONA (natural/jurídica)', 'ALARMAS']
+    columnas_deseadas = ['No. DOCUMENTO DE IDENTIDAD', 'NOMBRE', 'VALOR DE LA TRANSACCION', 'PROMEDIO', 'MEDIO PAGO', 'TIPO PERSONA (natural/jurídica)', 'ALARMAS']
     df_filtrado = df[df['ALARMAS'].isin(['INUSUAL', 'SOSPECHOSA'])]
     df_filtrado = df_filtrado[columnas_deseadas]
     fecha_actual = timezone.now()
